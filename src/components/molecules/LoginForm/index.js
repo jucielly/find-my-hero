b6c84cfedadcfb8bd44abac05b1cd7dce57b7d1f
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-05-24 21:25:37 -0300

    added auth on login form

--- src/components/molecules/LoginForm/index.js
+++ src/components/molecules/LoginForm/index.js
@@ -4,22 +4,25 @@ import styled from 'styled-components'
 import EmailInput from '../../molecules/EmailInput'
 import PasswordInput from '../../molecules/PasswordInput'
 import { useForm } from 'react-hook-form'
-
+import { useAuth } from '../../../providers/auth'
 
 
 
 const LoginForm = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log('data',data);
-    return (
-      
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <EmailInput color="defaultWhite" label="email"  register={register} required/>
-                <PasswordInput placeholder="senha" color="defaultWhite" label="password"  register={register} required/>
-                <Button type="submit">LOGIN</Button>
-            </Form>
+    const { login } = useAuth()
 
-        
+
+    const onSubmit = data => login(data.email, data.password)
+    return (
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <EmailInput color="defaultWhite" label="email" register={register} required />
+            <PasswordInput placeholder="senha" color="defaultWhite" label="password" register={register} required />
+            <Button type="submit">LOGIN</Button>
+        </Form>
+
+
     );
 }
 
